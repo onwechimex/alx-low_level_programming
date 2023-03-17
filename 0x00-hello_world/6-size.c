b1fa245e80300c29f3684bc5048690cb9d9d5be8
onwechimex <onwechimex@gmail.com>
2023-03-16 17:42:38 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,12 @@
  */
 int main(void)
 {
+	char d;
+	int a;
+	long int b;
+	long long int c;
+	float f;
+
 	printf("Size of a char: %lu bytes(s)\n", sizeof(d));
 	printf("Size of an int: %lu bytes(s)\n", sizeof(a));
 	printf("Size of long int: %lu bytes(s)\n", sizeof(b));
