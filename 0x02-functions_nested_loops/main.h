9e6d67b873e09ffc6efb3fa9ad261e5ee34b1fbf
onwechimex <onwechimex@gmail.com>
2023-03-21 22:55:21 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,14 +2,14 @@
 #define MAIN_H
 
 
-void print_ alphabet(void);
+void print_alphabet_(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(ints);
+int _abs(int);
 int print_last_digits(int);
-void jack_ bauer(void);
+void jack_bauer_(void);
 void times_tables(void);
 int add(int, int);
 void print_to_98(int n);
