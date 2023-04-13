f5eaa2a289af132ab74a9f5c3ffda1252f60bf5c
onwechimex <onwechimex@gmail.com>
2023-04-13 14:25:01 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * malloc_checked - array for prints a string
+ * @b: number of memory
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *n;
+n = malloc(b);
+if (n == NULL)
+{
+exit(98);
+}
+
+return (n);
+}
