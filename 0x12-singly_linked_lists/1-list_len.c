b3f6da0ba121dd58b24d4fc56e99bc749a21cc8d
onwechimex <onwechimex@gmail.com>
2023-04-27 14:08:59 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+* list_len - function that returns the number of elements in a
+* linked list_t list
+* @h: linked list
+* Return: returns the number of elements in h
+*/
+size_t list_len(const list_t *h)
+{
+	int x = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+			x++;
+		h = h->next;
+	}
+	return (x);
+}
